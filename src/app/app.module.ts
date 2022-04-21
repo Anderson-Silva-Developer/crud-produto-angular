108dbe58fba2c1163bc30cf5e969b3c170a89460
asc <silvaandersonpi@gmail.com>
2022-04-21 11:12:44 -0300

    add component home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,7 +9,9 @@ import {MatToolbarModule} from "@angular/material/toolbar";
 import { FooterComponent } from './components/template/footer/footer.component';
 import { NavComponent } from './components/template/nav/nav.component';
 import {MatSidenavModule} from '@angular/material/sidenav'
-import {MatListModule} from '@angular/material/list'
+import {MatListModule} from '@angular/material/list';
+import {MatCardModule} from '@angular/material/card';
+import { HomeComponent } from './views/home/home.component'
 
 @NgModule({
   declarations: [
@@ -17,6 +19,7 @@ import {MatListModule} from '@angular/material/list'
     HeaderComponent,
     FooterComponent,
     NavComponent,
+    HomeComponent,
 
   ],
   imports: [
@@ -25,7 +28,8 @@ import {MatListModule} from '@angular/material/list'
     BrowserAnimationsModule,
     MatToolbarModule,
     MatSidenavModule,
-    MatListModule
+    MatListModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
