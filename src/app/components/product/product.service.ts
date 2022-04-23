f4815b95d5d699679968e540f9a07b2ec2647332
asc <silvaandersonpi@gmail.com>
2022-04-23 11:27:56 -0300

    conclusão de update e delete

--- src/app/components/product/product.service.ts
+++ src/app/components/product/product.service.ts
@@ -27,13 +27,18 @@ export class ProductService {
   read():Observable<Product[]>{
     return this.http.get<Product[]>(this.baseUrl);
   }
-  readById(id:string):Observable<Product>{
+
+  readById(id: string | null):Observable<Product>{
     const url=`${this.baseUrl}/${id}`;
     return this.http.get<Product>(url);
   }
   update(product:Product):Observable<Product>{
     const url=`${this.baseUrl}/${product.id}`;
     return this.http.put<Product>(url,product);
+  }
+  delete(id:string | null):Observable<Product>{
+    const url=`${this.baseUrl}/${id}`;
+    return this.http.delete<Product>(url);
   }
 
 }
